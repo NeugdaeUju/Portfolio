f2e8ba651c82f03e1a2c6490fdc08ddb8cdd9a28
Leïla Pelletier <leila.plltr@gmail.com>
2025-12-08 15:09:27 +0100

    Création de la page de détails d'une réalisation (sans mise en page)

--- src/pages/WorksDetails.jsx
+++ src/pages/WorksDetails.jsx
@@ -1,6 +1,50 @@
+import { useParams } from "react-router-dom"
+import data from '../assets/data/project.json'
+import Header from '../components/Header'
+
 function WorksDetails () {
+    const {id} = useParams();
+    const project = data.find(project => project.id === id)
+
     return (
-        <h1>Page de détails</h1>
+        <>
+            <Header page={project.name}/>
+            <div className='worksDetails-page'>
+                <div className='worksDetails-page__description'>
+                    <img className='worksDetails-page__description--image'/>
+                    <div className='worksDetails-page__description__texts'>
+                        <h2 className='worksDetails-page__description__texts--title'>
+                            Projet : {project.name}
+                        </h2>
+                        <p className='worksDetails-page__description__texts--subtitles'>Description : </p>
+                        <p className='worksDetails-page__description__texts--text'>Lorem ipsum</p>
+                    </div>
+
+                </div>
+                <div className='worksDetails-page__process'>
+                    <h2 className='worksDetails-page__process--title'>
+                        Process :
+                    </h2>
+                    <img className='worksDetails-page__process--image'/>
+
+                </div>
+                <div className='worksDetails-page__skills'>
+                    <h2 className='worksDetails-page__skills--title'>Skills</h2>
+                    <div className='worksDetails-page__skills__list'>
+                        <p className='worksDetails-page__skills__list--item'>1</p>
+                        <p className='worksDetails-page__skills__list--item'>2</p>
+                        <p className='worksDetails-page__skills__list--item'>3</p>
+                        <p className='worksDetails-page__skills__list--item'>4</p>
+                        <p className='worksDetails-page__skills__list--item'>5</p>
+                        <p className='worksDetails-page__skills__list--item'>6</p>
+                        <p className='worksDetails-page__skills__list--item'>7</p>
+                        <p className='worksDetails-page__skills__list--item'>8</p>
+                        <p className='worksDetails-page__skills__list--item'>9</p>
+                    </div>
+
+                </div>
+            </div>
+        </>
     )
 }
 
