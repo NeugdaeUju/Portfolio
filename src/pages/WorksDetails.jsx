68c7abb870909f6ab60eecab54b31e0f1a4ba352
Leïla Pelletier <leila.plltr@gmail.com>
2025-12-08 15:27:45 +0100

    Mise en page de la page des détails des réalisations (description, images et skills temporaires)

--- src/pages/WorksDetails.jsx
+++ src/pages/WorksDetails.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom"
 import data from '../assets/data/project.json'
 import Header from '../components/Header'
+import '../assets/sass/worksDetails.scss'
 
 function WorksDetails () {
     const {id} = useParams();
@@ -13,33 +14,31 @@ function WorksDetails () {
                 <div className='worksDetails-page__description'>
                     <img className='worksDetails-page__description--image'/>
                     <div className='worksDetails-page__description__texts'>
-                        <h2 className='worksDetails-page__description__texts--title'>
+                        <h2 className='worksDetails-page__description__texts--title worksDetails-page__subtitles'>
                             Projet : {project.name}
                         </h2>
-                        <p className='worksDetails-page__description__texts--subtitles'>Description : </p>
+                        <h3 className='worksDetails-page__description__texts--subtitles'>Description : </h3>
                         <p className='worksDetails-page__description__texts--text'>Lorem ipsum</p>
                     </div>
 
                 </div>
                 <div className='worksDetails-page__process'>
-                    <h2 className='worksDetails-page__process--title'>
+                    <h2 className='worksDetails-page__process--title  worksDetails-page__subtitles'>
                         Process :
                     </h2>
                     <img className='worksDetails-page__process--image'/>
 
                 </div>
                 <div className='worksDetails-page__skills'>
-                    <h2 className='worksDetails-page__skills--title'>Skills</h2>
+                    <h2 className='worksDetails-page__skills--title  worksDetails-page__subtitles'>Skills</h2>
                     <div className='worksDetails-page__skills__list'>
-                        <p className='worksDetails-page__skills__list--item'>1</p>
-                        <p className='worksDetails-page__skills__list--item'>2</p>
-                        <p className='worksDetails-page__skills__list--item'>3</p>
-                        <p className='worksDetails-page__skills__list--item'>4</p>
-                        <p className='worksDetails-page__skills__list--item'>5</p>
-                        <p className='worksDetails-page__skills__list--item'>6</p>
-                        <p className='worksDetails-page__skills__list--item'>7</p>
-                        <p className='worksDetails-page__skills__list--item'>8</p>
-                        <p className='worksDetails-page__skills__list--item'>9</p>
+                        <p className='worksDetails-page__skills__list--item'>React</p>
+                        <p className='worksDetails-page__skills__list--item'>HTML</p>
+                        <p className='worksDetails-page__skills__list--item'>CSS</p>
+                        <p className='worksDetails-page__skills__list--item'>Redux</p>
+                        <p className='worksDetails-page__skills__list--item'>Doc Swagger</p>
+                        <p className='worksDetails-page__skills__list--item'>API</p>
+                        <p className='worksDetails-page__skills__list--item'>Git / GitHub</p>
                     </div>
 
                 </div>
