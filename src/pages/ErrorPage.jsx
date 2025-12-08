32ab7fc246b968bef441043306f8754fa2d3bb6a
Leïla Pelletier <leila.plltr@gmail.com>
2025-12-08 16:31:40 +0100

    Mise en page de la page d'accueil

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,12 +1,13 @@
 import { Link } from 'react-router-dom'
+import '../assets/sass/errorPage.scss'
 
 function ErrorPage () {
     return (
         <>
-        <div>
-            <h1>Erreur 404</h1>
-            <p>La page que vous avez demandé n'existe pas ou plus</p>
-            <Link to='/'>Cliquez ici pour retourner à la page d'accueil</Link>
+        <div className='error-page'>
+            <h1 className='error-page__title'>Erreur 404</h1>
+            <p className='error-page__text'>La page que vous avez demandé n'existe pas ou plus</p>
+            <Link to='/' className='error-page__link'>Cliquez ici pour retourner à la page d'accueil</Link>
         </div>
         </>
     )
