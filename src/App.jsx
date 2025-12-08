e689b678d750d3114f8d98d345f5f769dcf67838
Leïla Pelletier <leila.plltr@gmail.com>
2025-12-08 11:59:27 +0100

    Création des routes du site

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Footer from './components/Footer'
+import Accueil from './pages/Accueil'
+import Works from './pagesWorks'
+import WorksDetails from './pages/WorksDetails'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import ErrorPage from './pages/ErrorPage'
+
 function App() {
   return (
     <>
-      <h1>Hello, Porfolio</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Accueil />} />
+          <Route path='/works' element={<Works />} />
+          <Route path='/works/:id' element={<WorksDetails />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='*' element={<ErrorPage />} />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
     </>
   )
 }
