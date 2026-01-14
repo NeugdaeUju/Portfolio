edc0e6fb8f889614db537e8d1ed981f5168f949a
Leïla Pelletier <diane.arghat@gmail.com>
2026-01-14 14:37:17 +0100

    Modification de la page d'accueil et ajustmeent du layout et de index

--- src/pages/Accueil.jsx
+++ src/pages/Accueil.jsx
@@ -4,20 +4,21 @@ import '../assets/sass/accueil.scss'
 function Accueil() {
     return (
         <>
-            <main className='page-accueil'>
+            <div className='page-accueil'>
                 <div className='page-accueil__presentation'>
-                    <h1 className='page-accueil__presentation--title'>Leïla Pelletier</h1>
+                    <h1 className='page-accueil__presentation--title'>Leïla <br/>Pelletier</h1>
+                    <h2 className='page-accueil__presentation--role'><span className='page-accueil__presentation--role-span'>Intégratrice web / développeuse front junior</span>, spécialisée en HTML, CSS, JavaScript et React.</h2>
                     <p className='page-accueil__presentation--description'>
-                        Intégrateur web passionné, je conçois des expériences en ligne fluides et
-                        accessibles, pensées pour les utilisateurs.
+                        Je conçois et intègre des interfaces web accessibles, performantes et responsives à partir de maquettes.
+                        <br/>
+                        J’ai travaillé sur plusieurs projets concrets impliquant intégration React, consommation d’API et gestion de versions avec Git.
                     </p>
                 </div>
                 <nav className='page-accueil__nav'>
-                    <NavLink to='/works' className='page-accueil__nav--link'>Works</NavLink>
-                    <NavLink to='/about' className='page-accueil__nav--link'>About</NavLink>
-                    <NavLink to='/contact' className='page-accueil__nav--link'>Contact</NavLink>
+                    <NavLink to='/works' className='page-accueil__nav--link'>Réalisations</NavLink>
+                    <NavLink to='/about' className='page-accueil__nav--link'>A propos</NavLink>
                 </nav>
-            </main>
+            </div>
         </>
     )
 }
