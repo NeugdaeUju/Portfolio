e4904de758874af105ff503ee2cc8f2c8e3f0802
Leïla Pelletier <diane.arghat@gmail.com>
2026-01-14 14:22:14 +0100

    Correction sur le footer (ajout des liens de contact)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,14 @@
 import '../assets/sass/footer.scss'
+import { Link } from 'react-router-dom'
 
 function Footer () {
     return (
         <footer className='footer'>
-            <p className='footer_text'>@2025 - Leïla Pelletier</p>
+            <p className='footer__text'>@2025 - Leïla Pelletier</p>
+            <div className='footer__contact-links'>
+                <Link to="https://www.linkedin.com/in/le%C3%AFla-pelletier-672572249/" className='footer__contact-links__link'>LinkedIn</Link>
+                <Link to="https://github.com/neugdaeUju" className='footer__contact-links__link'>GitHub</Link>
+            </div>
         </footer>
     )
 }
