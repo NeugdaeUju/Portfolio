d9d386fdff0ddab5f5ee14c9f915ea20214f0bf1
Leïla Pelletier <diane.arghat@gmail.com>
2026-01-14 15:14:39 +0100

    Correction footer : ajout de target:_black pour renvoyer les liens de connexion vers un nouvel onglet

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,8 +6,8 @@ function Footer () {
         <footer className='footer'>
             <p className='footer__text'>@2025 - Leïla Pelletier</p>
             <div className='footer__contact-links'>
-                <Link to="https://www.linkedin.com/in/le%C3%AFla-pelletier-672572249/" className='footer__contact-links__link'>LinkedIn</Link>
-                <Link to="https://github.com/neugdaeUju" className='footer__contact-links__link'>GitHub</Link>
+                <Link to="https://www.linkedin.com/in/le%C3%AFla-pelletier-672572249/" className='footer__contact-links__link' target='_blank'>LinkedIn</Link>
+                <Link to="https://github.com/neugdaeUju" className='footer__contact-links__link' target='_blank'>GitHub</Link>
             </div>
         </footer>
     )
