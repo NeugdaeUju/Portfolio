0c7cc37c9f6f5a97017827be992c3be4dd95c458
Leïla Pelletier <diane.arghat@gmail.com>
2026-01-14 15:01:37 +0100

    Correction header : mise en gras de la page acutellement visitée

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,29 @@ import {NavLink} from 'react-router-dom'
 import '../assets/sass/header.scss'
 
 function Header () {
+
     return (
         <>
             <header className='header'>
                 <nav className='header__nav'>
-                    <NavLink to='/' className='header__nav--link'>Home</NavLink>
-                    <NavLink to='/works' className='header__nav--link'>Works</NavLink>
-                    <NavLink to='/about' className='header__nav--link'>About</NavLink>
+                    <NavLink to='/'
+                             className={({ isActive }) =>
+                             isActive ? "header__nav--link-active" : "header__nav--link"
+                             } end>
+                            Home
+                    </NavLink>
+                    <NavLink to='/works' 
+                             className={({ isActive }) =>
+                             isActive ? "header__nav--link-active" : "header__nav--link"
+                             } end>
+                            Works
+                    </NavLink>
+                    <NavLink to='/about'
+                             className={({ isActive }) =>
+                             isActive ? "header__nav--link-active" : "header__nav--link"
+                             } end>
+                            About
+                    </NavLink>
                 </nav>
             </header>
         </>
@@ -16,3 +32,41 @@ function Header () {
 }
 
 export default Header
+
+/*
+function Header() {
+  return (
+    <header>
+      <nav>
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive ? "nav-link active" : "nav-link"
+          }
+          end
+        >
+          Accueil
+        </NavLink>
+
+        <NavLink
+          to="/projects"
+          className={({ isActive }) =>
+            isActive ? "nav-link active" : "nav-link"
+          }
+        >
+          Projets
+        </NavLink>
+
+        <NavLink
+          to="/contact"
+          className={({ isActive }) =>
+            isActive ? "nav-link active" : "nav-link"
+          }
+        >
+          Contact
+        </NavLink>
+      </nav>
+    </header>
+  );
+}
+  */
