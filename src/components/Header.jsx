8ab0a97141a20bd7fccd43974446038e9008c144
Leïla Pelletier <leila.plltr@gmail.com>
2026-02-13 12:29:51 +0100

    MAJ Header : indiquer dans le menu où l'on se situe (works / about ...)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,14 +5,22 @@ function Header ({ hideHeader = ['/']}) {
     const location = useLocation()
     const showHeader = !hideHeader.includes(location.pathname)
 
+    const work = location.pathname === '/works'
+        ? 'active'
+        : ""
+    
+    const about = location.pathname === '/about'
+        ? 'active'
+        : ""
+
     return (
         <>
             <header className='header'>
                 {showHeader &&(
                 <nav className='header__nav'>
-                    <NavLink to='/' className='header__nav--link'>Home</NavLink>
-                    <NavLink to='/works' className='header__nav--link'>Works</NavLink>
-                    <NavLink to='/about' className='header__nav--link'>About</NavLink>
+                    <NavLink to='/' className='header__nav--link home'>Home</NavLink>
+                    <NavLink to='/works' className='header__nav--link works'>Works</NavLink>
+                    <NavLink to='/about' className='header__nav--link about'>About</NavLink>
                 </nav>
                 )}
             </header>
