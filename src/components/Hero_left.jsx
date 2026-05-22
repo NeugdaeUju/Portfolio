5fd38216cf26560a338cb5bc8cb0955be7c1563c
Leïla Pelletier <leila.plltr@gmail.com>
2026-05-22 09:16:00 +0200

    Rectification : création des fichiers dédié au hero_left

---
+++ src/components/Hero_left.jsx
@@ -0,0 +1,25 @@
+import '../assets/sass/hero_left.scss';
+
+function Hero_left () {
+    return (
+        <div className='hero-prez'>
+            <p className='hero-prez-text'>Disponible Full Remote</p>
+            <h1 className='hero-prez-title'>Développeuse <br/> <em>frontend</em><br/> & RH</h1>
+            <p className='hero-prez-description'>Je construit des <strong>interfaces qui résolvent de vrais problèmes métier</strong> - avec une compréhension opérationnelle des enjeux humains et organisationnels que peu de devs ont.</p>
+            <div className='hero-prez-tags'>
+                <span className='hero-prez-tags-badge badge-dev'>React Vue</span>
+                <span className='hero-prez-tags-badge badge-dev'>TypeScript</span>
+                <span className='hero-prez-tags-badge badge-rh'>RH & Admin</span>
+                <span className='hero-prez-tags-badge badge-rh'>Recrutement</span>
+                <span className='hero-prez-tags-badge badge-neutre'>Full remote</span>
+            </div>
+            <div className='hero-prez-links'>
+                <a href='#project' className='hero-prez-links-button project'>Voir mes projets</a>
+                <a href='#contact' className='hero-prez-links-button contact'>Me contacter</a>
+            </div>
+        </div>
+    )
+}
+
+export default Hero_left
+
--- src/pages/Accueil.jsx
+++ src/pages/Accueil.jsx
@@ -1,29 +1,14 @@
 import {NavLink} from 'react-router-dom'
 import '../assets/sass/accueil.scss'
 
+import Hero_left from '../components/Hero_Left';
+
 function Accueil() {
     return (
         <>
             <main className='page-accueil'>
                 <section className='hero'>
-                    <div className='hero-prez'>
-                        <p className='hero-prez-text'>Disponible Full Remote</p>
-                        <h1 className='hero-prez-title'>Développeuse <br/> <em>frontend</em><br/> & RH</h1>
-                        <p className='hero-prez-description'>Je construit des <strong>interfaces qui résolvent de vrais problèmes métier</strong> - avec une compréhension opérationnelle des enjeux humains et organisationnels que peu de devs ont.</p>
-                        <div className='hero-prez-tags'>
-                            <span className='hero-prez-tags-badge badge-dev'>React Vue</span>
-                            <span className='hero-prez-tags-badge badge-dev'>TypeScript</span>
-                            <span className='hero-prez-tags-badge badge-rh'>RH & Admin</span>
-                            <span className='hero-prez-tags-badge badge-rh'>Recrutement</span>
-                            <span className='hero-prez-tags-badge badge-neutre'>Full remote</span>
-                        </div>
-                        <div className='hero-prez-links'>
-                            <a href='#project' className='hero-prez-links-button project'>Voir mes projets</a>
-                            <a href='#contact' className='hero-prez-links-button contact'>Me contacter</a>
-                        </div>
-                    </div>
-
-
+                    <Hero_left />
                     <div className='hero-visual'>
                         <div>
                             <h2>LP</h2>
