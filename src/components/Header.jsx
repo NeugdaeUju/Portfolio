6dcd5a20340bd63276c842b9078c0dc6b11a478c
Leïla Pelletier <leila.plltr@gmail.com>
2026-06-02 10:40:47 +0200

    Responsive du Header avec création du menu Hamburger

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,22 @@
+import { useState } from 'react';
+import { FaBars, FaXmark } from "react-icons/fa6";
 import '../assets/sass/header.scss'
 
 function Header () {
-    
+    const [menuOpen, setMenuOpen] = useState(false);
     return (
         <>
             <header className='header'>
                 <a href="#" className='header__logo'>LP<span className='span'>Développeuse · RH</span></a>
-                <nav className='header__nav'>
+                <button className="hamburger" onClick={() => setMenuOpen(!menuOpen)}><FaBars /></button>
+                <nav className={`header_nav ${menuOpen ? 'active' : ''}`}>
+                    <button className="menu_close" onClick={() => setMenuOpen(false)}><FaXmark /></button>
                     <ul>
-                        <li><a className='header__nav__item' href='#about'>A propos</a></li>
-                        <li><a className='header__nav__item' href='#skills'>Compétences </a></li>
-                        <li><a className='header__nav__item' href='#projects'>Projets</a></li>
-                        <li><a className='header__nav__item' href='#experience'>Expérience</a></li>
-                        <li><a className='header__nav__button' href='#contact'>Me contacter</a></li>
+                        <li><a className='header__nav__item' href='#about' onClick={() => setMenuOpen(false)}>A propos</a></li>
+                        <li><a className='header__nav__item' href='#skills' onClick={() => setMenuOpen(false)}>Compétences </a></li>
+                        <li><a className='header__nav__item' href='#projects' onClick={() => setMenuOpen(false)}>Projets</a></li>
+                        <li><a className='header__nav__item' href='#experience' onClick={() => setMenuOpen(false)}>Expérience</a></li>
+                        <li><a className='header__nav__button' href='#contact' onClick={() => setMenuOpen(false)}>Me contacter</a></li>
                     </ul>
                 </nav>
             </header>
