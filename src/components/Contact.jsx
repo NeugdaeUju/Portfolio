b035bce53e206417bdf0b9191865343ae2346fd3
Leïla Pelletier <leila.plltr@gmail.com>
2026-06-04 08:53:08 +0200

    Ajout du CV (+ correction couleur header)

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -14,9 +14,9 @@ function Contact () {
                 <p className='sub'>Je recherche une équipe qui avance, qui scale, et qui valorise un profil capable de coder et de comprendre les enjeux humains derrière le produit.</p>
             </div>
             <div className='action'>
-                <a className='CV button' href='mailto:email@exemple.com'>Envoyer un mail</a>
+                <a className='CV button' href='mailto:leila.plltr@gmail.com'>Envoyer un mail</a>
                 <a className='link button' href='https://www.linkedin.com/in/le%C3%AFla-pelletier-672572249/' target='_blank'>Linkedin</a>
-                <a className='link button' href='#'>Télécharger le CV</a>
+                <a className='link button' href='../../public/documents/CV_Leila_Pelletier.pdf' download>Télécharger le CV</a>
             </div>
         </div>
         </>
